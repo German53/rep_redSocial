c6a97b4c0e304b8e4ce414c631a9d64161c7d421
German53 <germanmilitello025@gmail.com>
2026-01-07 20:21:39 -0300

    Create CommentController.php

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function save(Request $request){
+        $validate = $this->validate($request, [
+            'image_id' => 'integer|required',
+            'content' => 'string|required'
+        ]);
+
+        $user = Auth::user();
+        $image_id = $request->input('image_id');
+        $content = $request->input('content');
+
+        $comment = new Comment();
+        $comment->user_id = $user->id;
+        $comment->image_id = $image_id;
+        $comment->content = $content;
+        
+        $comment->save();
+
+        return redirect()->route('image.detail', ['id' => $image_id]);
+    }
+    public function delete($id){
+        $user = Auth::user();
+
+        $comment = Comment::find($id);
+        if ($user && ($comment->user_id == $user->id || $comment->image->user_id == $user->id)) {
+            $comment->delete();
+
+            return redirect()->route('image.detail', ['id' => $comment->image->id])->with(['message' => 'Comentario eliminado correctamente']);
+        }else{
+            return redirect()->route('image.detail', ['id' => $comment->image->id]);
+        }
+    }
+}
